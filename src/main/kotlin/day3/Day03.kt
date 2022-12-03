7083b73e30440c7becaa73d42242020834bdf5c7
MikeLBook <mikebook24@gmail.com>
2022-12-03 17:22:00 -0600

    update day 3 solution

--- src/main/kotlin/day3/Day03.kt
+++ src/main/kotlin/day3/Day03.kt
@@ -3,11 +3,11 @@ package day3
 import splitCorrectly
 import java.io.File
 
-val itemTypes = arrayOf(
-    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
-    "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
-    "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
-)
+// inspired by Maia Grotepass on the Kotlin by JetBrains channel
+// much cleaner than my manually written string List
+fun Char.getPriority(): Int {
+    return if (this.isUpperCase()) this.code - 'A'.code + 27 else this.code - 'a'.code + 1
+}
 
 fun main() {
     val rucksacks = File("src/main/kotlin/day3/day03.txt").readLines()
@@ -17,18 +17,21 @@ fun main() {
            val rucksackItems = it.splitCorrectly()
            val firstCompartment = rucksackItems.subList(0, it.length / 2)
            val secondCompartment = rucksackItems.subList(it.length / 2, it.length)
-           val intersection = firstCompartment.intersect(secondCompartment.toSet()).first()
-           itemTypes.indexOf(intersection) + 1
+           firstCompartment.intersect(secondCompartment.toSet())
+               .first()
+               .single()
+               .getPriority()
        }
     }
 
     fun part2(): Int {
         return rucksacks.windowed(3, 3) {
-            val intersection = it[0].splitCorrectly()
+            it[0].splitCorrectly()
                 .intersect(it[1].splitCorrectly().toSet())
                 .intersect(it[2].splitCorrectly().toSet())
                 .first()
-            itemTypes.indexOf(intersection) + 1
+                .single()
+                .getPriority()
         }.sum()
     }
 
