4cae967d5011c91c9c85af4f41a587db37b13c69
MikeLBook <mikebook24@gmail.com>
2022-12-04 15:29:37 -0600

    extract out common logic

--- src/main/kotlin/day4/Day04Destructuring.kt
+++ src/main/kotlin/day4/Day04Destructuring.kt
@@ -8,28 +8,6 @@ import java.io.File
     and a custom operator to be able to use 'in' keyword between two IntRanges
  */
 
-val input = File("src/main/resources/day4.txt").readLines()
-
-fun main() {
-    val part1 = input.map {
-        val (a, b) = it.split(",")
-        a.toRange() to b.toRange()
-    }.count { (a, b) ->
-        a in b || b in a
-    }
-
-    println(part1)
-
-    val part2 = input.map {
-        val (a, b) = it.split(",")
-        a.toRange() to b.toRange()
-    }.count { (a, b) ->
-        a.any { it in b }
-    }
-
-    println(part2)
-}
-
 fun String.toRange(): IntRange {
     val (start, end) = split("-")
     return start.toInt()..end.toInt()
@@ -37,4 +15,24 @@ fun String.toRange(): IntRange {
 
 operator fun IntRange.contains(other: IntRange): Boolean {
     return other.first >= this.first && other.last <= this.last
+}
+
+val input = File("src/main/resources/day4.txt").readLines()
+
+fun main() {
+    val ranges: List<Pair<IntRange, IntRange>> = input.map {
+        val (a, b) = it.split(",")
+        a.toRange() to b.toRange()
+    }
+
+    val part1 = ranges.count { (a, b) ->
+        a in b || b in a
+    }
+
+    val part2 = ranges.count { (a, b) ->
+        a.any { it in b }
+    }
+
+    println("Day 04 part 1 solution: $part1")
+    println("Day 04 part 2 solution: $part2")
 }
