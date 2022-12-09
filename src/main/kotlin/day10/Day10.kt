1e8488a4702f3ef8cd511e5b06f596f1b0db9050
MikeLBook <mikebook24@gmail.com>
2022-12-09 17:50:07 -0600

    prep day 10 files

---
+++ src/main/kotlin/day10/Day10.kt
@@ -0,0 +1,14 @@
+package day10
+
+import java.io.File
+
+class Day10(private val lines: List<String>) {
+    
+}
+
+fun main() {
+    val day10 = Day10(File("src/main/resources/day10Sample.txt").readLines())
+
+    println("Day 10 part 1 solution: ")
+    println("Day 10 part 2 solution: ")
+}
