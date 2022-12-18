e30f05e2c4060c0673f4e647469345907659736a
Michael <mikeb@phase2online.com>
2022-12-18 01:20:10 -0600

    add day 13 solution

--- src/main/kotlin/day13/Day13.kt
+++ src/main/kotlin/day13/Day13.kt
@@ -2,35 +2,85 @@ package day13
 
 import java.io.File
 
-class Day13(private val lines: List<String>) {
-    fun part1() {
-        val test = lines.filter { it != "" }.chunked(2) {
-            val first = listParser(it.first())
-            val second = listParser(it.last())
-            listOf(first, second)
+class Day13(lines: List<String>) {
+    private val packetPairs = lines.chunked(2) { listOf(listParser(it.first()), listParser(it.last())) }
+    private val packets = lines.map { listParser(it) }
+
+    fun part1(): Int = packetPairs.mapIndexedNotNull { index, (left, right) ->
+        if (pairsAreInRightOrder(left, right) == 1) index + 1 else null
+    }.sum()
+
+    fun part2(): Int {
+        val dividerPackets = listOf(listOf(listOf(2)), listOf(listOf(6)))
+        val comparator = { a: List<*>, b: List<*> ->
+            pairsAreInRightOrder(a, b)
         }
-        println(test)
+        return (dividerPackets + packets)
+            .sortedWith(comparator)
+            .reversed()
+            .mapIndexed { index, it -> if (dividerPackets.contains(it)) index+1 else 1 }
+            .reduce(Int::times)
     }
 
-    private fun listParser(stringList: String): List<Any> {
+    private fun pairsAreInRightOrder(left: List<*>, right: List<*>): Int {
+        val indices = if (left.size > right.size) left.indices else right.indices
+
+        indices.forEach { i ->
+            val leftE = left.getOrNull(i) ?: return 1
+            val rightE = right.getOrNull(i) ?: return -1
+
+            if (leftE is Int && rightE is Int) when {
+                leftE < rightE -> return 1
+                leftE > rightE -> return -1
+                else -> return@forEach
+            } else {
+                val subListComparison = when {
+                    leftE is Int -> pairsAreInRightOrder(listOf(leftE), rightE as List<*>)
+                    rightE is Int -> pairsAreInRightOrder(leftE as List<*>, listOf(rightE))
+                    else -> pairsAreInRightOrder(leftE as List<*>, rightE as List<*>)
+                }
+                if (subListComparison == 0) return@forEach else return subListComparison
+            }
+        }
+
+        return 0
+    }
+
+    private fun listParser(stringList: String): List<*> {
         return buildList {
             var nestingLevel = 0
-            val tempListMap: MutableMap<Int, MutableList<Int>> = mutableMapOf()
-            stringList.forEach { character ->
-                if (character == '[') {
-                    nestingLevel += 1
-                    tempListMap[nestingLevel] = mutableListOf()
-                } else if (character == ']') {
-                    if (nestingLevel != 1) {
-                        add(tempListMap[nestingLevel]!!)
+            val tempListMap: MutableMap<Int, MutableList<Any>> = mutableMapOf()
+            stringList.forEachIndexed { index, character ->
+                when {
+                    character == '[' -> {
+                        nestingLevel += 1
+                        tempListMap[nestingLevel] = mutableListOf()
+                    }
+                    character == ']' -> {
+                        if (nestingLevel == 2) {
+                            add(tempListMap[nestingLevel]!!)
+                        } else if (nestingLevel > 2) {
+                            tempListMap[nestingLevel - 1]?.add(tempListMap[nestingLevel]!!)
+                        }
                         tempListMap.remove(nestingLevel)
                         nestingLevel -= 1
                     }
-                } else if (character.isDigit()) {
-                    if (nestingLevel == 1) {
-                        add(character.toString().toInt())
-                    } else {
-                        tempListMap[nestingLevel]?.add(character.toString().toInt())
+                    character.isDigit() -> {
+                        val prevChar = stringList[index - 1]
+                        when {
+                            prevChar.isDigit() && nestingLevel == 1 -> {
+                                val previousElement = removeLast().toString()
+                                val newNumber = previousElement + character
+                                add(newNumber.toInt())
+                            }
+                            prevChar.isDigit() -> {
+                                val previousElement = tempListMap[nestingLevel]?.removeLast().toString()
+                                val newNumber = previousElement + character
+                                tempListMap[nestingLevel]?.add(newNumber.toInt())
+                            }
+                            nestingLevel == 1 -> add(character.toString().toInt())
+                            else -> tempListMap[nestingLevel]?.add(character.toString().toInt())
+                        }
                     }
                 }
             }
@@ -39,7 +89,7 @@ class Day13(private val lines: List<String>) {
 }
 
 fun main() {
-    val day13 = Day13(File("src/main/resources/day13Sample.txt").readLines())
+    val day13 = Day13(File("src/main/resources/day13.txt").readLines().filter { it != "" })
     println("Day 13 part 1 solution: ${day13.part1()}")
-    println("Day 13 part 2 solution: ")
+    println("Day 13 part 2 solution: ${day13.part2()}")
 }
